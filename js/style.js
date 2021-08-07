029b238e1f173c5109c07115ceb9c445e97476f7
Arben Peposi <arben.peposi@outlook.fr>
2021-08-07 16:06:17 +0200

    ajout url de mes projets + redaction de mon parcours

--- js/style.js
+++ js/style.js
@@ -1,13 +1,20 @@
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+        $('.header').animate({ height: "100px" });
+        $('nav').animate({
+            height: "100px"
+        });
+
     } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
     }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
+$('#return-to-top').click(function() { // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+        scrollTop: 0 // Scroll to top of body
     }, 500);
+    $('.header').css('height', '150px');
+    $(this).css('transition-delay', '1s');
 });
