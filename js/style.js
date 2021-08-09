bae0dd2f97ba7dfaac723b7010685442da09e04b
Arben Peposi <arben.peposi@outlook.fr>
2021-08-09 15:14:18 +0200

    ajout d'une autre timeline + hover effect sur les images

--- js/style.js
+++ js/style.js
@@ -3,18 +3,36 @@ $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200); // Fade in the arrow
         $('.header').animate({ height: "100px" });
+        $('.header').stop().animate({ height: "100px" });
         $('nav').animate({
             height: "100px"
         });
 
     } else {
         $('#return-to-top').fadeOut(200); // Else fade out the arrow
+        $('.header').stop().animate({ height: "100px" });
+        $('.header').animate({ height: "150px" });
+
     }
-});
-$('#return-to-top').click(function() { // When arrow is clicked
-    $('body,html').animate({
-        scrollTop: 0 // Scroll to top of body
-    }, 500);
-    $('.header').css('height', '150px');
-    $(this).css('transition-delay', '1s');
+    $('#return-to-top').click(function() { // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0 // Scroll to top of body
+        }, 500);
+
+        $('#go-down').click(function() { // When arrow is clicked
+            $('html, body').animate({
+                scrollTop: $("#profil").offset().top
+            }, 1500);
+        });
+    });
+
+    // A $( document ).ready() block.
+
+
+    // Fade in the arrow
+    if ($('#go-down').scrollTop() >= 1) {
+        $(this).fadeOut(200);
+    }
+
+
 });
