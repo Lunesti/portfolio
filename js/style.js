8fb78a45ebfa8c6eff6f0991a1dc991ac57da0de
Arben Peposi <arben.peposi@outlook.fr>
2021-08-10 10:32:00 +0200

    scroll menu

--- js/style.js
+++ js/style.js
@@ -1,38 +1,27 @@
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
+    let height_ = "100px";
     if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200); // Fade in the arrow
-        $('.header').animate({ height: "100px" });
-        $('.header').stop().animate({ height: "100px" });
+
         $('nav').animate({
             height: "100px"
         });
 
     } else {
         $('#return-to-top').fadeOut(200); // Else fade out the arrow
-        $('.header').stop().animate({ height: "100px" });
-        $('.header').animate({ height: "150px" });
+        height_ = "150px";
 
     }
-    $('#return-to-top').click(function() { // When arrow is clicked
-        $('body,html').animate({
-            scrollTop: 0 // Scroll to top of body
-        }, 500);
-
-        $('#go-down').click(function() { // When arrow is clicked
-            $('html, body').animate({
-                scrollTop: $("#profil").offset().top
-            }, 1500);
-        });
-    });
+    $('.header').stop().animate({ "height": height_ }, 100);
 
     // A $( document ).ready() block.
 
+});
 
-    // Fade in the arrow
-    if ($('#go-down').scrollTop() >= 1) {
-        $(this).fadeOut(200);
-    }
-
+$('#return-to-top').click(function() { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
 
 });
