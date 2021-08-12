c68f2225a063469df3425a2be9a8044fdc08ba1b
Arben Peposi <arben.peposi@outlook.fr>
2021-08-12 17:59:34 +0200

    réglage du scroll menu en responsive et nouveau style dans la section projet

--- js/style.js
+++ js/style.js
@@ -2,22 +2,18 @@
 
 $(window).scroll(function() {
 
-    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200); // Fade in the arrow
-        $('nav').stop().animate({ height: "50px" }); // ici j'ai mis 50px, c'est bien suffisant
-
-
-    } else {
-        $('#return-to-top').fadeOut(200); // Else fade out the arrow
-        $('nav').stop().animate({ height: "150px" });
-        if ($(window).width() <= 768) {
+    if ($(this).width() >= 768) // tablet + desktop
+    {
+        if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200); // Fade in the arrow
             $('nav').stop().animate({ height: "50px" });
+        } else {
+            $('#return-to-top').fadeOut(200); // Else fade out the arrow
+            $('nav').stop().animate({ height: "150px" });
         }
-
     }
 
 });
-// A $( document ).ready() block.
 
 
 $('#return-to-top').click(function() { // When arrow is clicked
